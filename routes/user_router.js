c296a4ae741e2612ae49e9446dadd022d764eb47
Sai-Chandu-544 <pendotasaichandu1234@gmail.com>
2025-08-04 11:22:36 +0530

    Update user_router.js

--- routes/user_router.js
+++ routes/user_router.js
@@ -1,6 +1,6 @@
 const express=require("express")
 const router=express.Router()
-const {user_register,user_login,user_profile,user_post,all_posts}=require("../controllers/userLogic")
+const {user_register,user_login,user_profile,user_post,all_posts,user_posts}=require("../controllers/userLogic")
 
 const middleware=require("../middleware/middleware")
 
@@ -9,5 +9,6 @@ router.post("/login",user_login)
 router.post("/post",middleware,user_post)
 router.get("/profile",middleware,user_profile)
 router.get("/allposts",middleware,all_posts)
+router.get("/:userId",middleware,user_posts)
 
 module.exports=router;
